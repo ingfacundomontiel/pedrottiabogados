9787e6c6a0ac50e320e55783f528812d160a8a2b
facundomontiel <facundomontiel22@gmail.com>
2023-08-28 10:49:05 -0300

    Pedrotti Abogados Theme: Upload

---
+++ wp-content/themes/pedrotti-abogados/header.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Pedrotti_Abogados
+ */
+
+if ( is_front_page() ) {
+	$nav_logo_link = '#top';	
+} else {
+	$nav_logo_link = 'https://pedrottiabogados.com';	
+}
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
+		<div id="page" class="site">
+			<header id="masthead" class="site-header">
+				<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+					<div class="container px-4">
+						<a href="<?php echo $nav_logo_link ?>" class="logo-navegacion">
+							PEDROTTI ABOGADOS
+						<a>
+						<button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+
+						<div class="collapse navbar-collapse" id="main-menu">
+							<?php
+							wp_nav_menu(array(
+								'theme_location' => 'menu-principal',
+								'container' => false,
+								'menu_class' => '',
+								'fallback_cb' => '__return_false',
+								'items_wrap' => '<ul id="%1$s" class="navbar-nav ms-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+								'depth' => 2,
+								'walker' => new bootstrap_5_wp_nav_menu_walker()
+							));
+							?>
+						</div>
+					</div>
+				</nav>
+			</header><!-- #masthead -->
---
+++ wp-content/themes/pedrotti-abogados/page-homepage.php
@@ -0,0 +1,359 @@
+<?php
+// Template Name: Homepage
+get_header();
+?>
+
+<span class='nav-anchor' id='top'></span>
+<section class="intro-hero">
+	<div class='container-fluid'>
+		<div class="row border-white border-bottom">
+			<div class="col-12 col-md-8 hero-left titulos-principales-col">
+				<img src="http://pedrottiabogados.com/wp-content/uploads/2022/02/banner-header-apellidos.png" alt='Pedrotti Abogados'>
+			</div>
+			<div class="col-12 col-md-4 nombres-col px-0 p-md-0 py-5 justify-content-end">
+				<div class="text-left nombres">
+					<h4>
+						Daniel F. Pedrotti
+					</h4>
+					<h4>
+						Carla A. Bassani
+					</h4>
+					<h4>
+						Eladio S. Oronel
+					</h4>
+				</div>
+				<div class="info">
+					Asesores legales desde 1989
+				</div>
+			</div>
+		</div>
+<!-- 		<div class="row">
+			<div class="col-12 col-md-5 border-white col-inferior col-inferior-1">
+				Sargento Cabral 143, Rafaela
+			</div>
+			<div class="col-12 col-md-3 border-white col-inferior col-inferior-2 white-no-style">
+				<p class="my-0">
+					3492 424248
+				</p>			
+			</div>
+			<div class="col-12 col-md-4 col-inferior col-inferior-3">
+				
+			</div>
+		</div> -->
+	</div>
+</section>
+
+<section id='frase-destacada' class='seccion-contenido'> 
+	<h4 class="texto-destacado text-center">
+		Derecho a tus derechos
+	</h4>
+</section>
+
+<span class='nav-anchor' id='nosotros'></span>
+<section id='nosotros-seccion' class='seccion-contenido'>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2 class="titulo">
+					Nosotros
+				</h2>
+				<p class="descripcion">
+					Acreditamos más de 30 años de trayectoria. Nos caracteriza una cultura de trabajo, donde destacan la calidad en el servicio y el compromiso y responsabilidad de los profesionales que integran el Estudio, como una atención inmediata y personalizada, por medio de un equipo que aborda cada consulta o situación de conflicto traído por el cliente, con el mayor profesionalismo y seriedad, buscando brindar un <strong>consejo legal eficaz</strong>.
+				</p>
+				<p class="descripcion">
+					También una reconocida experiencia en encuadramientos jurídicos adecuados que repercuten en la anticipación o prevención del conflicto, así como las habilidades conciliatorias y de negociación de sus abogados. Trabajamos tanto en planificación preventiva como en la gestión contenciosa, partiendo de un análisis profundo para el diseño estratégico y jurídico, trazado por nuestros valores.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<span class='nav-anchor' id='profesionales'></span>
+<section id='profesionales-seccion' class='seccion-contenido'>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 titulos">
+				<h2 class="titulo">
+					Nuestro equipo de abogados
+				</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-4">
+				<img class="img-profesional" src="https://pedrottiabogados.com/wp-content/uploads/2021/11/daniel-f-pedrotti.png" alt="Daniel F. Pedrotti">
+				<h4 class="nombre-profesional">
+					Daniel F. Pedrotti
+				</h4>
+				<p class="profesion-profesional">
+					ABOGADO
+				</p>
+				<p class="descripcion-profesional">
+					Especializado en derecho del trabajo y a la salud, contratos civiles y comerciales, la reparación de daños y fraudes, derechos de consumidores y usuarios, la estructuración jurídica de negocios inmobiliarios.
+				</p>
+				<div class="collapse" id='daniel-pedrotti-trayectoria' aria-expanded='false'>
+					<ul class="card-text trayectoria-profesional ps-0 ms-4">						
+						<li>Abogado: Facultad de Ciencias Jurídicas y Sociales, Universidad Nacional del Litoral – Egresado 1984;</li>
+						<li>Profesor Ayudante de Cátedra Derecho Civil II (Derechos de las Obligaciones), Facultad de Ciencias Jurídicas y Sociales, Universidad Nacional del Litoral (1992 – 1997)</li>
+						<li>Profesor Adjunto Derecho Civil II (Derechos de las Obligaciones), Facultad de Ciencias Jurídicas y Sociales, Universidad Nacional del Litoral  (1997 – 2005)</li>
+						<li>Asesor Legal Secretaría de Estado de Trabajo y Seguridad Social (1987 - 1996); </li>
+						<li>"Estudio Jurídico Asociado" de los Dres. Celso Trivelli y Ricardo L. Lorenzetti (1986-1989); </li>
+						<li class="text-decoration-underline"><strong>"Estudio Jurídico Lorenzetti – Pedrotti" (1989 – 1998)</strong></li>
+						<li>“PEDROTTI Abogados” (1998 – 2021)</li>
+						<li>Programa de Dirección de Pequeñas y Medianas Empresas – DPME 2008  -  IAE Business School </li>
+						<li>Mat. Fº 283 – Tomo III – Exp. 4085 . CSJN Tomo 92 – Folio 217.</li>
+					</ul>
+				</div>
+
+				<button class="btn btn-primary mt-2 mb-5 px-0 trayectoria-button" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#daniel-pedrotti-trayectoria" aria-expanded="false" aria-controls="daniel-pedrotti-trayectoria">Trayectoria profesional></button>
+
+			</div>
+			<div class="col-12 col-md-4">
+				<img class="img-profesional" src="https://pedrottiabogados.com/wp-content/uploads/2021/11/carla-bassani.png" alt="Carla Bassani">
+				<h4 class="nombre-profesional">
+					Carla Bassani
+				</h4>
+				<p class="profesion-profesional">
+					ABOGADA
+				</p>
+				<p class="descripcion-profesional">
+					Especializada en el derecho de comercio y la empresa, las sociedades y el sin número de conflictos e intereses vinculados a las mismas, en el campo de las conciliaciones como en el proceso judicial.
+				</p>
+				<div class="collapse" id='carla-bassani-trayectoria' aria-expanded='false'>
+					<ul class="card-text trayectoria-profesional ps-0 ms-4">
+						<li>Abogado en “ESTUDIO JURÍDICO PEDROTTI” - 1.999 a la fecha.</li>
+						<li>Trade Development Institute – TDI - www.tdigroup.ie;   Programa de reconversión industrial para la Argentina,  Rosario, Santa Fe. Área: Gerenciamiento del plan PYMEXPORTA de asistencia a Pymes en su proceso exportador.
+							Tarea desempeñada: Asistente de Ejecutivo de Cuentas
+							Años  1997 - 1998
+						</li>
+						<li>“Estudio Jurídico Dogliani”, Rosario, Santa Fe, año 1998. Especialización: derecho empresario</li>
+						<li>Pasantía en asesoramiento legal de sociedades (régimen de constitución, transformación, fusión y extinción).</li>
+						<li>“Estudio Contable  Cristini”,  Rosario, Santa Fe. Área: Dpto. de Sociedades – año 1997;</li>
+						<li>Master en Asesoramiento Jurídico de Empresas; Facultad de Ciencias Empresariales, Universidad Austral Rosario  - Egresada año  2001;  Tesis evaluada con “Sobresaliente”;</li>
+						<li>Egresada año  1997  Matrícula profesional: Libro XXVI F° 306</li>
+						<li>Abogada - Facultad de Derecho, Universidad Nacional de Rosario </li>
+						<li>Idiomas:  dominio oral y escrito del inglés y alemán.</li>
+					</ul>
+				</div>
+
+				<button class="btn btn-primary mt-2 mb-5 px-0 trayectoria-button" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#carla-bassani-trayectoria" aria-expanded="false" aria-controls="carla-bassani-trayectoria">Trayectoria profesional></button>
+			</div>
+
+			<div class="col-12 col-md-4">
+				<img class="img-profesional" src="https://pedrottiabogados.com/wp-content/uploads/2021/11/eladio-s-oronel.png" alt="Eladio S. Oronel">
+				<h4 class="nombre-profesional">
+					Eladio S. Oronel
+				</h4>
+				<p class="profesion-profesional">
+					ABOGADO
+				</p>
+				<p class="descripcion-profesional">
+					Especializado en el derecho del trabajo (individual y colectivo), la reparación de daños por accidentes de trabajo o tránsito, las cuestiones vinculadas a derechos reales y del dominio (compraventas, usufructos, división de condominio, constitución de Fideicomisos).
+				</p>
+				<div class="collapse" id='eladio-oronel-trayectoria' aria-expanded='false'>
+					<ul class="card-text trayectoria-profesional ps-0 ms-4">
+						<li>Abogado en “ESTUDIO JURÍDICO PEDROTTI” – 2005 a la fecha.</li>
+						<li>TESCRIBANÍA FEDERIK.- Escribana Olga Fernández Gonzalo de Federik.-
+							Funciones: Asesor Jurídico, Trámites Registrales, Bancarios y  Tributarios.- Años 2003 -  2004.
+						</li>
+						<li>Profesor Adscripto Docente Institución: Universidad Católica Argentina.- Facultad de Derecho.- Subsede Paraná.- Derecho Civil II (Derecho de las Obligaciones).- Año: 2005 – 2007. Titular: Dr. Luis Niel Puig.
+						</li>
+						<li>Practicante Docente (Resol. 235-04 - CD) - Universidad Nacional del Litoral.- Facultad de Ciencias Jurídicas y Sociales - Derecho Civil II (Derecho de las Obligaciones).- Año: 2004 - 2007/</li>
+						<li>Docente Ayudante Alumno (por concurso abierto de oposición y antecedentes - Resol. 684-01-CD.)   Universidad Nacional del Litoral.- Facultad de Ciencias Jurídicas y Sociales.- Materia Concursada: Derecho Civil II (Derecho de las Obligaciones).- Año: Marzo 2002- Febrero 2003.- Calificación: Primer Puesto.</li>
+						<li>Notario. Abogado Especialista en Derecho Notarial y Registral – Egresado Año 2004.</li>
+						<li>Colegio de Abogados de Rafaela (Mat. Fº85 - TºI - Expte.5-337)</li>
+						<li>Abogado: Facultad de Ciencias Jurídicas y Sociales, Universidad Nacional del Litoral – Egresado 2002.</li>
+						<li>Idiomas: dominio oral y escrito del inglés.</li>
+					</ul>
+				</div>
+
+				<button class="btn btn-primary mt-2 mb-5 px-0 trayectoria-button" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#eladio-oronel-trayectoria" aria-expanded="false" aria-controls="eladio-oronel-trayectoria">Trayectoria profesional></button>
+			</div>
+		</div>
+	</div>
+</section>
+
+<span class='nav-anchor' id='servicios'></span>
+<section id='servicios-seccion' class='seccion-contenido'>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 titulos">
+				<h2 class="titulo">
+					Áreas de práctica
+				</h2>
+			</div>
+		</div>
+
+		<div class="row fila-3-servicios py-3">
+			<div class="col-12 col-md-3 py-1 p-md-0">
+				<div class="card servicio border-0">
+					<img src="https://pedrottiabogados.com/wp-content/uploads/2021/11/derecho-civil.png" class="card-img-top" alt="Derecho Civil">
+					<div class="card-body">
+						<h5 class="card-title text-center">DERECHO CIVIL</h5>
+						<div class="collapse" id='derecho-civil' aria-expanded='false'>
+							<p class="card-text text-center py-2">Acreditamos amplia experiencia en la negociación y resolución extrajudicial (conciliaciones, mediaciones) y la tramitación judicial en todo tipo de conflictos y procesos, y en todas sus instancias procesales.<br>
+								Redacción y negociación de contratos; constitución  de asociaciones y sociedades, modificaciones estatutarias y disolución; compraventa de inmuebles urbanos y rurales, arrendamiento y locaciones; locación de obra; cobro de pesos y persecución de deudores fraudulentamente insolventes; resarcimiento de daños patrimoniales y extrapatrimoniales, etc.
+							</p>
+						</div>
+						<center>
+							<button class="btn btn-primary mt-3" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#derecho-civil" aria-expanded="false" aria-controls="derecho-civil">Leer más></button>
+						</center>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-3 py-1 p-md-0">
+				<div class="card servicio border-0">
+					<img src="https://pedrottiabogados.com/wp-content/uploads/2021/11/derecho-danios.png" class="card-img-top" alt="Derecho Civil">
+					<div class="card-body">
+						<h5 class="card-title text-center">DERECHO DE DAÑOS</h5>
+						<div class="collapse" id='derecho-danios' aria-expanded='false'>
+							<p class="card-text text-center">Reconocida trayectoria en procesos de resarcimientos de daños extrapatrimoniales y patrimoniales.<br>
+								Comprensiva de áreas tan diversas como: responsabilidad profesional; acciones civiles en accidentes de trabajo; accidentes de tránsito; daños sufridos en espectáculos públicos; derivados de la actividad del Estado; derivados de fraudes o tentativas de fraudes; querellas;  responsabilidad de miembros de Asociaciones Civiles y Mutuales, socios y Directores de Sociedades Anónimas; responsabilidad precontractual e  incumplimiento de contratos; daños derivados de divorcios; falta de reconocimiento oportuno de filiación, etc.
+							</p>
+						</div>
+						<center>
+							<button class="btn btn-primary mt-3" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#derecho-danios" aria-expanded="false" aria-controls="derecho-danios">Leer más></button>
+						</center>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-3 py-1 p-md-0">
+				<div class="card servicio border-0">
+					<img src="https://pedrottiabogados.com/wp-content/uploads/2021/11/derecho-comercial.png" class="card-img-top" alt="Derecho Civil">
+					<div class="card-body">
+						<h5 class="card-title text-center">DERECHO COMERCIAL Y DE LA EMPRESA</h5>
+						<div class="collapse" id='derecho-comercial' aria-expanded='false'>
+							<p class="card-text text-center">Redacción, negociación e interpretación de contratos comerciales; sociedades, 
+								joint ventures, agrupaciones de colaboración, UTE, Asociaciones y otras formas asociativas, como su disolución; transferencias de empresas y fondos de comercio.<br>
+								Contratos comerciales de servicios y de colaboración: Agencia, Distribución, Licencias, etc. Área del Derecho del Trabajo: encuadramiento, contratos, negociaciones de convenios, actuación ante Autoridades Administrativas, acciones judiciales individuales y colectivas.
+
+							</p>
+						</div>
+						<center>
+							<button class="btn btn-primary mt-3" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#derecho-comercial" aria-expanded="false" aria-controls="derecho-comercial">Leer más></button>
+						</center>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row py-3">
+			<div class="col-12 col-md-3 py-1">
+				<div class="card servicio border-0">
+					<img src="https://pedrottiabogados.com/wp-content/uploads/2021/11/derecho-laboral.png" class="card-img-top" alt="Derecho Civil">
+					<div class="card-body">
+						<h5 class="card-title text-center">DERECHO LABORAL</h5>
+						<div class="collapse" id='derecho-laboral' aria-expanded='false'>
+							<p class="card-text text-center">Reconocida trayectoria en la atención de conflictos del Trabajo, individuales y colectivos. Representación permanente de diversos Sindicatos y sus afiliados.</p>
+						</div>
+						<center>
+							<button class="btn btn-primary mt-3" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#derecho-laboral" aria-expanded="false" aria-controls="derecho-laboral">Leer más></button>
+						</center>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-3 py-1">
+				<div class="card servicio border-0">
+					<img src="https://pedrottiabogados.com/wp-content/uploads/2021/11/derecho-familia.png" class="card-img-top" alt="Derecho Civil">
+					<div class="card-body">
+						<h5 class="card-title text-center">DERECHO DE FAMILIA</h5>
+						<div class="collapse" id='derecho-familia' aria-expanded='false'>
+							<p class="card-text text-center py-2">Atención de controversias en Derecho de familia; Asesoramiento en constitución y cese de uniones convivenciales y pactos de división de bienes; Tramitación de divorcios unilaterales y contenciosos; Asesoramiento y representación en trámites de liquidación y partición de la comunidad de ganancias; Determinación y reclamos por compensación económica;  Daños en las relaciones de familia.
+							</p>
+						</div>
+						<center>
+							<button class="btn btn-primary mt-3" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#derecho-familia" aria-expanded="false" aria-controls="derecho-familia">Leer más></button>
+						</center>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-3 py-1">
+				<div class="card servicio border-0">
+					<img src="https://pedrottiabogados.com/wp-content/uploads/2021/11/derechos-reales-fideicomiso.png" class="card-img-top" alt="Derecho Civil">
+					<div class="card-body">
+						<h5 class="card-title text-center">DERECHO REALES FIDEICOMISOS</h5>
+						<div class="collapse" id='derecho-real' aria-expanded='false'>
+							<p class="card-text text-center">Asesoramiento a clientes (inversores, desarrolladores, empresas constructoras y profesionales participantes) en proyectos inmobiliarios en la ciudad y la Provincia.<br>
+								Desarrollo de la estructura  legal, conforme leyes y reglamentos nacionales, provinciales y municipales y para la estructuración del financiamiento que facilite la realización.
+							</p>
+						</div>
+						<center>
+							<button class="btn btn-primary mt-3" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#derecho-real" aria-expanded="false" aria-controls="derecho-real">Leer más></button>
+						</center>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-3 py-1 py-md-0">
+				<div class="card servicio border-0">
+					<img src="https://pedrottiabogados.com/wp-content/uploads/2021/11/fraudes.png" class="card-img-top" alt="Derecho Civil">
+					<div class="card-body">
+						<h5 class="card-title text-center">FRAUDES</h5>
+						<div class="collapse" id='derecho-fraude' aria-expanded='false'>
+							<p class="card-text text-center">Investigación extrajudicial (asistida por empresas y profesionales especializados subcontratados al efecto) en supuestos ocultamiento malicioso de bienes para eludir compromisos patrimoniales o situaciones de responsabilidad civil.</p>
+						</div>
+						<center>
+							<button class="btn btn-primary mt-3" type="button" role="button" class="collapsed" data-bs-toggle="collapse" href="#derecho-fraude" aria-expanded="false" aria-controls="derecho-fraude">Leer más></button>
+						</center>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="container-fluid d-none d-md-block nuestros-valores-banner"></div>
+<div class="container-fluid d-md-none px-0">
+	<img class="w-100" src="http://pedrottiabogados.com/wp-content/uploads/2021/12/banner-mobile.png" alt="Nuestros Valores">
+</div>
+
+<span class='nav-anchor' id='nuestros-valores'></span>
+<section id='nuestros-valores-seccion' class="seccion-contenido">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 titulos">
+				<h4 class="texto-destacado pt-4 text-center">
+					Inmediatez, compromiso y resultados
+				</h4>
+				<p class="descripcion text-center">
+					Nuestro propósito es ofrecer servicios profesionales desde el involucramiento y compromiso, generando proximidad y colaboración recíproca con el cliente en la atención de su conflicto y la superación o solución del mismo.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<span class='nav-anchor' id='contacto'></span>
+<section id='contacto-seccion' class="seccion-contenido">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 titulos">
+				<h2 class="titulo">
+					Escribinos para asesorarte
+				</h2>
+				<h3 class="subtitulo">
+					Contactanos
+				</h3>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-6 info-contacto">
+				<p>
+					Whatsapp: <strong>+54 (3492) 589090</strong>
+				</p>
+				<p>
+					Teléfono: <strong>(03492) 424248</strong>
+				</p>
+				<a href="mailto:secretaria@pedrottiabogados.com">secretaria@pedrottiabogados.com</a>
+				<p class="mt-3">
+					Sargento Cabral 143, Rafaela, Santa Fe
+				</p>
+			</div>
+			<div class="col-12 col-md-6 px-4 px-md-0">
+				<?php echo do_shortcode('[contact-form-7 id="31" title=""]'); ?>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php
+get_footer();
+?>
